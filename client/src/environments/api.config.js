34c0dd3b862374c4562be4f5f2e88c698e65f79a
jagallob <jagallob@eafit.edu.co>
2025-06-21 15:17:32 -0500

    feat: alternar entre API remota y local mediante variable de entorno

--- client/src/environments/api.config.js
+++ client/src/environments/api.config.js
@@ -1,3 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:7086",
+  BASE_URL:
+    (import.meta.env.VITE_API_USE_LOCAL === "true"
+      ? import.meta.env.VITE_API_BASE_URL_LOCAL
+      : import.meta.env.VITE_API_BASE_URL) || "http://localhost:7086",
 };
